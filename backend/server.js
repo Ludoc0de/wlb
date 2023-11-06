0c5ec1dcdd5ba5563c232585f5a8b1e63c27fe5a
ludoc0de <champi.ludo@tutanota.com>
2023-11-06 16:58:12 -0300

    up

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require("path");
 require("dotenv").config({ path: "./backend/config/.env" });
 const indexRoutes = require("./routes/index");
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use("/api", indexRoutes);
 // static files
-app.use(express.static(path.join(__dirname, "../frontend/build/index.html")));
+app.use(express.static(path.join(__dirname, "../frontend/build/")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
