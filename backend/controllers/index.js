2898e907cff6262fe599ae41c6222ad226be4fb1
ludoc0de <champi.ludo@tutanota.com>
2023-11-08 18:57:37 -0300

    up

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -5,24 +5,6 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const YOUR_DOMAIN = "https://wlb.onrender.com/";
 
 module.exports = {
-  // memoryPayment: async (req, res) => {
-  //   console.log("in");
-  //   YOUR_DOMAIN;
-  //   const session = await stripe.checkout.sessions.create({
-  //     line_items: [
-  //       {
-  //         price: "price_1Looa6E79bJvVWvwUI5q0fGO",
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     mode: "payment",
-  //     allow_promotion_codes: true,
-  //     success_url: `${YOUR_DOMAIN}?success=true`,
-  //     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-  //   });
-  //   res.redirect(303, session.url);
-  // },
-  // testPayement
   memoryPayment: async (req, res) => {
     YOUR_DOMAIN;
     const session = await stripe.checkout.sessions.create({
@@ -39,4 +21,21 @@ module.exports = {
     });
     res.redirect(303, session.url);
   },
+  // testPayement
+  // memoryPayment: async (req, res) => {
+  //   YOUR_DOMAIN;
+  //   const session = await stripe.checkout.sessions.create({
+  //     line_items: [
+  //       {
+  //         price: "price_1Looa6E79bJvVWvwUI5q0fGO",
+  //         quantity: 1,
+  //       },
+  //     ],
+  //     mode: "payment",
+  //     allow_promotion_codes: true,
+  //     success_url: `${YOUR_DOMAIN}?success=true`,
+  //     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  //   });
+  //   res.redirect(303, session.url);
+  // },
 };
