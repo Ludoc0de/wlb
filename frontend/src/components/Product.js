ff598a313c4d23d7f4dca886841a0a1b57a1ab7a
ludoc0de <champi.ludo@tutanota.com>
2023-11-10 19:40:46 -0300

    up font image

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,28 +1,23 @@
 export default function Product() {
   return (
     <section
-      className="hero min-h-screen flex py-80 md:py-40 pl-4"
+      className="hero flex py-80 md:py-44 pl-4"
       style={{
-        backgroundImage: "url(/wlbHero1.jpg)",
+        backgroundImage: "url(/header.jpeg)",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "right center",
-        backgroundSize: "100% 100%",
+        backgroundSize: "50%",
       }}
     >
       <div className="hero-content text-left text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-6xl font-bold text-gray-100">
-            Women luxury beauty
-          </h1>
+          <h1 className="mb-5 text-6xl  text-gray-100">Women luxury beauty</h1>
           <p className="mb-10 text-xl text-gray-100">
             Studio d’extension de cils et de rehaussement de cils situé au cœur
             de Rémire Montjoly.
           </p>
           <form action="/api/checkout" method="POST">
-            <button
-              type="submit"
-              className="btn bg-yellow-500 text-gray-800 py-4 px-8"
-            >
+            <button type="submit" className="btn btn-lg gold-wlb text-black">
               Acompte
             </button>
           </form>
