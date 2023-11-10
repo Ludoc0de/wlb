19bda1eef54ecd41bed1635fb0da3c251534c531
ludoc0de <champi.ludo@tutanota.com>
2023-11-10 07:35:49 -0300

    sticky

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="navbar bg-base-100 bg-white text-black">
+    <div className="navbar bg-base-100 bg-white text-black sticky top-0">
       <div className="flex-1">
         {/* <a className="btn btn-ghost normal-case text-xl">WLB</a> */}
         <label
