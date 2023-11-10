d5c3a20ea254a741ddfbb5964d5b07f9759929c4
ludoc0de <champi.ludo@tutanota.com>
2023-11-09 22:45:24 -0300

    up btn

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,14 @@ export default function Header() {
     <div className="navbar bg-base-100 bg-white text-black">
       <div className="flex-1">
         {/* <a className="btn btn-ghost normal-case text-xl">WLB</a> */}
-        <div className="w-10">
-          <img src="/wlbIcon1.jpeg" />
-        </div>
+        <label
+          tabIndex={0}
+          className="btn btn-ghost bg-black btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img src="/wlbIcon1.jpeg" />
+          </div>
+        </label>
       </div>
       <div className="flex-none">
         <a className="btn btn-ghost normal-case text-xl">Contact</a>
