6f586c066e17664a8f9e79af4ca318aecb14dc99
ludoc0de <champi.ludo@tutanota.com>
2023-11-09 22:43:07 -0300

    color, logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 bg-white text-black">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">WLB</a>
+        {/* <a className="btn btn-ghost normal-case text-xl">WLB</a> */}
+        <div className="w-10">
+          <img src="/wlbIcon1.jpeg" />
+        </div>
       </div>
       <div className="flex-none">
         <a className="btn btn-ghost normal-case text-xl">Contact</a>
