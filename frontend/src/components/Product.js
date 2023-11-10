c7632be88197d62f4f3b07a2f071388b0cefc8fc
ludoc0de <champi.ludo@tutanota.com>
2023-11-09 22:21:55 -0300

    remove font

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -11,17 +11,17 @@ export default function Product() {
     >
       <div className="hero-content text-left text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-6xl font-bold text-gray-100 font-playfair">
+          <h1 className="mb-5 text-6xl font-bold text-gray-100">
             Women luxury beauty
           </h1>
-          <p className="mb-10 text-xl text-gray-100 font-roboto">
+          <p className="mb-10 text-xl text-gray-100">
             Studio d’extension de cils et de rehaussement de cils situé au cœur
             de Rémire Montjoly.
           </p>
           <form action="/api/checkout" method="POST">
             <button
               type="submit"
-              className="btn bg-yellow-500 text-gray-800 py-4 px-8 font-bold font-satisfy"
+              className="btn bg-yellow-500 text-gray-800 py-4 px-8"
             >
               Acompte
             </button>
